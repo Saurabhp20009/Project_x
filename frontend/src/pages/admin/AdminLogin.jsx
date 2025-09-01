34ad542a68e531323dd84f75e9510a94d1e8aa9e
Saurabh patwal <saurabhpatwal92000@gmail.com>
2025-09-01 18:25:52 +0530

    [Changes] code on AdminLogin frontend

--- frontend/src/pages/admin/AdminLogin.jsx
+++ frontend/src/pages/admin/AdminLogin.jsx
@@ -1,15 +1,43 @@
-import React from 'react'
+import React from "react";
+import "./AdminLogin.css";
 
 const AdminLogin = () => {
-  
-    const imageUrl = '/images/admin_login_background.webp';
-    console.log(imageUrl);
-    return (
-        <div>
-            <div style={{ backgroundImage: `url(${imageUrl})`, height: '100vh', backgroundSize: 'cover', backgroundPosition: 'center' }}>
-            </div>
-        </div>
-    );
-}
+  const imageUrl = "/images/admin_login_background.webp";
+  console.log(imageUrl);
+  return (
+    <div>
+      <div
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+          height: "100vh",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <div className="LoginFormDiv">
+          <header>Admin Login</header>
+          <div>
+            <form
+              id="Loginform"
+              onSubmit={(e) => {
+                e.preventDefault();
+                console.log("Form submitted");
+              }}
+            >
+              <div className="input_form">
+                <input type="text" name="username" placeholder="Username" />
+              </div>
+              <div className="input_form">
+                <input type="password" name="password" placeholder="Password" />
+              </div>
 
-export default AdminLogin
+              <button type="submit">Login</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminLogin;
